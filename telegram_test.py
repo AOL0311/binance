c92005086cd58805aef4765faffac536071dd25a
AOL0311 <104348861+AOL0311@users.noreply.github.com>
2025-08-14 21:59:31 +0800

    Update telegram_test.py

--- telegram_test.py
+++ telegram_test.py
@@ -1,9 +1,10 @@
 from telegram import Bot
 import asyncio
 
+# token, chat_id 暫時移除
 async def test():
-    bot = Bot(token = '7683441276:AAFJsaLJpNzDf23Nh4pBnjrPIAvKI0-gu5w')
-    chat_id = '6515758042'
+    bot = Bot(token = '')
+    chat_id = ''
     await bot.send_message(chat_id = chat_id, text = 'test')
 
 asyncio.run(test())
